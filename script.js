ecfd7486548bdbf621ffab7d833eb1ac8fddbdde
tomasdarienko <darienkotomas@gmail.com>
2023-11-20 16:04:58 +0100

    finito

---
+++ script.js
@@ -0,0 +1,26 @@
+const km = prompt ('inserisci i chilometri da percorrere')
+console.log(km)
+
+const eta = prompt ('inserisci la tua eta')
+console.log(eta)
+
+
+let prezzo = km*0.21
+
+
+
+if( eta<19){
+     prezzo = prezzo-prezzo*20/100
+    
+     console.log(n = prezzo.toFixed(2))
+}
+else{
+     prezzo = prezzo
+      
+     if( eta>64){
+          prezzo = prezzo-prezzo*40/100
+           
+     }
+     console.log(n = prezzo.toFixed(2))
+   
+}
